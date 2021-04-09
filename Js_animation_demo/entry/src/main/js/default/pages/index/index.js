400cdd7bef630a9788f419e5a025a4e3dd706a71
657943010@qq.com <657943010@qq.com>
2021-04-09 11:02:30 +0800

    Js_animation_demo组件

---
+++ Js_animation_demo/entry/src/main/js/default/pages/index/index.js
@@ -0,0 +1,13 @@
+// xxx.js
+export default {
+    data: {
+        colorParam: '',
+        opacityParam: '',
+    },
+    showAnimation: function () {
+        this.colorParam = '';
+        this.opacityParam = '';
+        this.colorParam = 'color';
+        this.opacityParam = 'opacity';
+    },
+}
