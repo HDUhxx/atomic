f2c4e333206042c45f702ae5581944683d39953c
657943010@qq.com <657943010@qq.com>
2021-05-26 10:43:26 +0800

    ceshi-1

---
+++ ceshi_1/jltfmoban10/entry/build/intermediates/shell/debug/src/main/java/com/example/jltfmoban10/SharedServiceShellService.java
@@ -0,0 +1,11 @@
+package com.example.jltfmoban10;
+
+import android.os.Bundle;
+import ohos.abilityshell.AbilityShellService;
+
+public class SharedServiceShellService extends AbilityShellService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
