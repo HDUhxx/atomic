bb5c8d051cbff1787a4ffd800106a83fcc931084
657943010@qq.com <657943010@qq.com>
2021-07-16 16:49:21 +0800

    ImmersiveInformation体验

---
+++ ImmersiveInformation/entry/src/main/java/com/jltf/jltfcardtemplate06/MyApplication.java
@@ -0,0 +1,10 @@
+package com.jltf.jltfcardtemplate06;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ ImmersiveInformation/entry/src/main/java/com/jltf/jltfcardtemplate06/widget/widget/WidgetImpl.java
@@ -0,0 +1,41 @@
+package com.jltf.jltfcardtemplate06.widget.widget;
+
+import com.jltf.jltfcardtemplate06.widget.controller.FormController;
+import ohos.aafwk.ability.AbilitySlice;
+import ohos.aafwk.ability.ProviderFormInfo;
+import ohos.aafwk.content.Intent;
+import ohos.app.Context;
+
+/**
+ * The main class related to immersion service_widget.
+ * When adding an immersion card in launcher, it will display a graphic and text interface in form widget.
+ * When clicking the immersion card, it will route to {@link WidgetSlice} and show a operation page used to
+ * display data of the immersion card.
+ * immersion card supports running in phone/tablet/wearable devices.
+ */
+public class WidgetImpl extends FormController {
+
+    public WidgetImpl(Context context, String formName, Integer dimension) {
+        super(context, formName, dimension);
+    }
+
+    @Override
+    public ProviderFormInfo bindFormData() {
+        return null;
+    }
+
+    @Override
+    public void updateFormData(long formId, Object... vars) {
+
+    }
+
+    @Override
+    public void onTriggerFormEvent(long formId, String message) {
+
+    }
+
+    @Override
+    public Class<? extends AbilitySlice> getRoutePageSlice(Intent intent) {
+        return WidgetSlice.class;
+    }
+}
