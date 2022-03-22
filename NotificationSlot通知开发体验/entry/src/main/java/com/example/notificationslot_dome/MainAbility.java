22b504314b7ea52b6fbe2b408e482a97202b4e0a
李洋水蛟龙 <657943010@qq.com>
2022-03-22 13:33:32 +0800

    NotificationSlot通知开发体验“”

---
+++ NotificationSlot通知开发体验/entry/src/main/java/com/example/notificationslot_dome/MainAbility.java
@@ -0,0 +1,13 @@
+package com.example.notificationslot_dome;
+
+import com.example.notificationslot_dome.slice.MainAbilitySlice;
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+    }
+}
